a4580bfd872edea1b8f01a5ff097d1d00cb472f4
x-lite <53637079+x-lite@users.noreply.github.com>
2021-12-26 23:42:51 +0000

    Restore '24/12/2021, 11:19:37 Image list'

--- functions.ts
+++ functions.ts
@@ -1,10 +1,24 @@
 namespace functions {
 
     let i = 0
+    let happyImg = images.iconImage(IconNames.Happy)
+    let sadImg = images.iconImage(IconNames.Sad)
+    let smallHeart = images.iconImage(IconNames.SmallHeart)
+    let heart = images.iconImage(IconNames.Heart)
+    let ghost = images.iconImage(IconNames.Ghost);
+
+    let imgList = [happyImg, sadImg, smallHeart, heart, ghost]
+
+    function loopThruImages() {
+        imgList.forEach(function(i){
+            i.showImage(0)
+            basic.pause(500)
+        })
+    }
 
     export function run() {
 
-        drawDiagonal();
+       loopThruImages();
         
     }
 
@@ -23,5 +37,42 @@ namespace functions {
         led.toggle(i, i)
         i++
         if(i > 4)i=0;
+    }
+
+
+    function collection() {
+        let list = [1, 5, 9]
+
+        for(let r = 0; r < 10; r++) {
+            
+            let x = list.indexOf(r)
+            
+            if (x > 0) {
+                happy();
+            } else {
+                sad();
+            }
+            basic.pause(1000);
+        }   
+    }
+
+
+
+    function happy() {
+        happyImg.showImage(0);
+    }
+    function sad() {
+        sadImg.showImage(0);
+    }
+
+    function image() {
+
+        input.onButtonPressed(Button.A, () => {
+            sad();
+        });
+        input.onButtonPressed(Button.B, () => {
+            happy();
+        });
+
     }
 }
