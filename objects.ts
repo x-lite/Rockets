f4471cbd14b65ef872e8e5e8ca24dcaaa4dfde62
x-lite <53637079+x-lite@users.noreply.github.com>
2021-12-27 16:35:18 +0000

    Ticking done

--- objects.ts
+++ objects.ts
@@ -1,104 +1,139 @@
 namespace objects {
 
     export function run() {
-        //runMyFirstObject();
-        //setOffASingleRocket();
-        basic.forever(setOffABunchOfRockets)
+        setOffABunchOfRockets();
     }
 
 
-    function runMyFirstObject() {
-        let myObject = new myFirstObject();
-        myObject.drawDiagonal();
-    }
-
-    function setOffASingleRocket() {
-        let rocket = new simpleRocket(0, "SIMPLE");
-        rocket.fire();
-    }
+    let lastTick = 0;
 
     function setOffABunchOfRockets() {
-        let rockets = [new simpleRocket(0, "COMPLEX"), new simpleRocket(1, "SIMPLE"), new simpleRocket(2, "COMPLEX"), new simpleRocket(3, "SIMPLE"), new simpleRocket(4, "SIMPLE")]
-     
+        let rockets = [new SimpleRocket(0, 250), new SimpleRocket(1, 400), new SimpleRocket(2, 800), new SimpleRocket(3, 0), new SimpleRocket(4, 600)]
         rockets.forEach(function(rocket) {
             rocket.fire();
         })
-    
-    }
 
-    class myFirstObject {
-
-        //i = 0;
-        constructor() {
-        }
-
-        drawDiagonal() {
-            functions.drawDiagonal();
-        }
+        basic.forever(function() {
            
+            basic.pause(50)
+            let delta = input.runningTime() - lastTick;
+            lastTick = input.runningTime();
+
+            rockets.forEach(function (rocket) {
+                rocket.tick(new TickInfo(delta))
+            })
+        })
     }
 
-    class simpleRocket {
+    class SimpleRocket {
 
         _xPosition: number;
-        _explosionType: string;
+        _yPosition: number;
+        _fired: boolean;
+        _flying: boolean;
+        _exploding: boolean;
+        _timeSinceLastMove: number;
+        _delayBetweenFrames: number;
 
-        constructor(xPosition: number, explosionType: string) {
+        constructor(xPosition: number, delayBetweenFrames: number) {
             this._xPosition = xPosition;
-            this._explosionType = explosionType;
+            this._yPosition = 4;
+            this._timeSinceLastMove = 0;
+            this._delayBetweenFrames = delayBetweenFrames;
             this.reset();
         }
 
+        tick(tick: TickInfo) {
+            if(this._fired) {
+                this.nextFiringStep(tick);
+            }
+        }
+
+        nextFiringStep(tick: TickInfo) {
+            if(this._flying) {
+                this.nextFlyingFrame(tick);
+            } else if(this._exploding) {
+                this.nextExplodingFrame(tick);
+            }
+        }
+
+        nextFlyingFrame(tick: TickInfo) {
+            //Check if enough time has passed before we start another frame
+            if (this._timeSinceLastMove + tick._delta < this._delayBetweenFrames) {
+                this._timeSinceLastMove = this._timeSinceLastMove + tick._delta;
+                return;
+            }
+
+            //reset the timer for next frame delay
+            this._timeSinceLastMove = 0;
+
+            //If rocket is still on screen then move up 1 position - else explode!
+            if (this._yPosition >= 0) {
+                this._yPosition--;
+                led.unplot(this._xPosition, this._yPosition + 1)
+                led.plot(this._xPosition, this._yPosition);
+            } else {
+                this._flying = false;
+                this._exploding = true;
+            }
+        }
+
+        _explosionPart1 = true;
+        
+        nextExplodingFrame(tick: TickInfo) {
+
+            //Check if enough time has passed before we start another frame
+            if (this._timeSinceLastMove + tick._delta < 250) {
+                this._timeSinceLastMove = this._timeSinceLastMove + tick._delta;
+                return;
+            }
+
+            //reset the timer for next frame delay
+            this._timeSinceLastMove = 0;
+
+            //Explode the rocket - part1
+            if(this._explosionPart1) {
+                led.plot(this._xPosition - 1, 0);
+                led.plot(this._xPosition, 0);
+                led.plot(this._xPosition + 1, 0);
+                led.plot(this._xPosition, 1)
+                this._explosionPart1 = false;
+            } else {
+                //Explode the rocket - part2
+                led.unplot(this._xPosition - 1, 0);
+                led.unplot(this._xPosition, 0);
+                led.unplot(this._xPosition + 1, 0);
+                led.unplot(this._xPosition, 1)
+            }
+
+        }
+
         reset() {
+            this._flying = false;
+            this._exploding = false;
             led.plot(this._xPosition, 4)
             led.unplot(this._xPosition, 0)
         }
 
         fire() {
-            this.flyUp();
-            this.explode();
-            basic.pause(250);
-            this.reset();
+            this._fired = true;
+            this._flying = true;
         }
-
-        flyUp() {
-            for (let ypos = 4; ypos >= 0; ypos--) {
-                led.unplot(this._xPosition, ypos + 1)
-                led.plot(this._xPosition, ypos);
-                basic.pause(250);
-            }
-        }
-
-        explode() {
-            if(this._explosionType=="SIMPLE") this.simpleExplosion();
-            if(this._explosionType=="COMPLEX") this.complexExplosion();
-        }
-
-        simpleExplosion() {
-            //Explode the rocket - part1
-            led.plot(this._xPosition - 1, 0);
-            led.plot(this._xPosition, 0);
-            led.plot(this._xPosition + 1, 0);
-            led.plot(this._xPosition, 1)
-
-
-            basic.pause(250);
-
-            //Explode the rocket - part2
-            led.unplot(this._xPosition - 1, 0);
-            led.unplot(this._xPosition, 0);
-            led.unplot(this._xPosition + 1, 0);
-            led.unplot(this._xPosition, 1)
-        }
-
-        complexExplosion() {
-            this.simpleExplosion();
-            basic.pause(100);
-            this.simpleExplosion();
-            basic.pause(100);
-            this.simpleExplosion();
-        }
-
 
     }
+
+
+    class Explosion {
+
+    }
+
+    
+    class TickInfo {
+        public _delta: number;
+
+        constructor(delta: number) {
+            this._delta = delta;
+        }
+    }
+
 }
